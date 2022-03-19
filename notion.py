fb1ec12f445e498ff6bbb14dbc6ea608719ceb7b
Mathieu <mathieu@Mathieus-MBP-2.net>
2022-03-19 08:39:37 +0100

    first commit

---
+++ notion.py
@@ -0,0 +1,87 @@
+import requests
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(dotenv_path=BASE_DIR /".env")
+
+class Notion:
+    def __init__(self):
+        self.hed = {'Authorization': f'Bearer {os.getenv("NOTION_API_KEY")}',
+               'Notion-version': '2022-02-22'}
+
+    def search(self,data = None):
+        r = requests.post('https://api.notion.com/v1/search', json=data,headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+    def get_page_id(self):
+        r = requests.get('https://api.notion.com/v1/pages/575a4f383f034971b39f75e12b445a77', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+    def delete_page_id(self):
+        r = requests.delete('https://api.notion.com/v1/blocks/be4f2161f49647f59eaf1420f3a88568', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+
+    def get_bot(self):
+        r = requests.get('https://api.notion.com/v1/users/me', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+    def get_all_user(self):
+        r = requests.get('https://api.notion.com/v1/users?page_size=100', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+    def get_block_id(self):
+        r = requests.get('https://api.notion.com/v1/blocks/575a4f383f034971b39f75e12b445a77', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+
+    def get_db_id(self):
+        r = requests.get('https://api.notion.com/v1/databases/c9d39095c55444fdb055a30ef835214f', headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+
+    def createPage(self):
+        r = requests.post('https://api.notion.com/v1/pages', json={
+        "parent": { "database_id": "c9d39095c55444fdb055a30ef835214f" },
+        "properties": {
+            "Name": {
+    "title": [
+      {
+        "type": "text",
+        "text": {
+          "content": "The title"
+        }
+      }
+    ]
+  },
+
+
+"Status": {
+    "select":
+      {
+        "name": "PASSED"
+      }
+  }
+
+
+
+        }
+
+
+    },headers=self.hed)
+        print(r.status_code)
+        print(r.json())
+
+
+testcafe = Notion()
+testcafe.createPage()
